1d80546db4bf76ea1d0b586bcd20af0ae3272d19
PETEr Zhang <peterzhang@PETErdeAir.lan>
2026-02-11 17:56:13 +0000

    Chapter 4: Add templates for Rango index

--- rango/views.py
+++ rango/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Rango says hello world!")
-
+    context_dict = {}
+    return render(request, 'rango/index.html', context=context_dict)
